a7f6fb2dfee8c12d1faaacb7108982574fdc5a36
Ameer <47337834+Ameerprogrammer@users.noreply.github.com>
2025-01-09 00:36:18 -0500

    Code

---
+++ Sum_Of_Two_Numbers.java
@@ -0,0 +1,10 @@
+public class SumOfTwoNumbers {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("The sum of 2 numbers is: " + sum(3,1));
+    }
+}
